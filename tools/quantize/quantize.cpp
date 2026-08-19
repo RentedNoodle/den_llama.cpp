f2514fbf9832d3244ad2ba6f188b6556197ee117
RentedNoodle <rentednoodle@github>
2026-08-19 04:47:18 -0500

    feat(qwen35): MTP d2t draft-vocab trim support + quant requantize fixes (local checkpoint)

--- tools/quantize/quantize.cpp
+++ tools/quantize/quantize.cpp
@@ -59,6 +59,7 @@ static const std::vector<quant_option> QUANT_OPTIONS = {
     { "Q3_K_L",   LLAMA_FTYPE_MOSTLY_Q3_K_L,   " 4.03G, +0.5562 ppl @ Llama-3-8B",  },
     { "IQ4_NL",   LLAMA_FTYPE_MOSTLY_IQ4_NL,   " 4.50 bpw non-linear quantization", },
     { "IQ4_XS",   LLAMA_FTYPE_MOSTLY_IQ4_XS,   " 4.25 bpw non-linear quantization", },
+    { "NVFP4",    LLAMA_FTYPE_MOSTLY_NVFP4,    " 4-bit NVFP4 Blackwell (E4M3)",     },
     { "Q4_K",     LLAMA_FTYPE_MOSTLY_Q4_K_M,   "alias for Q4_K_M",                  },
     { "Q4_K_S",   LLAMA_FTYPE_MOSTLY_Q4_K_S,   " 4.37G, +0.2689 ppl @ Llama-3-8B",  },
     { "Q4_K_M",   LLAMA_FTYPE_MOSTLY_Q4_K_M,   " 4.58G, +0.1754 ppl @ Llama-3-8B",  },
